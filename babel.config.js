c67e824d3710ea40b4ae0ed7cbdf90cd261122f8
Joseph Roque <github@josephroque.ca>
2020-11-02 09:17:53 -0500

    Initial React Native app commit

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          "components": ["./src/components"],
+          "features": ["./src/features/"],
+          "navigation": ["./src/navigation"],
+          "utils": ["./src/utils/"]
+        }
+      }
+    ]
+  ]
+};
