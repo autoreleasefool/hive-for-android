91cd1cf164ee5210ffb93d2358d74fb621db47cd
Joseph Roque <github@josephroque.ca>
2020-11-09 20:42:21 -0500

    Tab feature flags

--- src/utilities/features.ts
+++ src/utilities/features.ts
@@ -5,6 +5,10 @@ export enum Feature {
   guestMode = 'Guest mode',
   offlineMode = 'Offline mode',
   accounts = 'Accounts',
+  profileTab = 'Profile tab',
+  historyTab = 'History tab',
+  lobbyTab = 'Lobby tab',
+  spectatorTab = 'Spectator tab',
 }
 
 enum Rollout {
@@ -13,31 +17,38 @@ enum Rollout {
   released,
 }
 
-const featureRollout = (feature: Feature) => {
+const featureRollout = (feature: Feature): Rollout => {
   switch (feature) {
     case Feature.guestMode:
+    case Feature.spectatorTab:
       return Rollout.released;
     case Feature.offlineMode:
       return Rollout.inDevelopment;
     case Feature.accounts:
+    case Feature.profileTab:
+    case Feature.historyTab:
+    case Feature.lobbyTab:
       return Rollout.disabled;
   }
 };
 
-const featureDependencies = (feature: Feature): Feature[] => {
+const featureDependencies = (feature: Feature): Set<Feature> => {
   switch (feature) {
     case Feature.accounts:
-      return [];
     case Feature.guestMode:
-      return [];
     case Feature.offlineMode:
-      return [];
+    case Feature.lobbyTab:
+    case Feature.spectatorTab:
+      return new Set();
+    case Feature.profileTab:
+    case Feature.historyTab:
+      return new Set([Feature.accounts]);
   }
 };
 
 const isFeatureEnabled = (feature: Feature) => {
   const rollout = featureRollout(feature);
-  const dependenciesEnabled = featureDependencies(feature)
+  const dependenciesEnabled = Array.from(featureDependencies(feature))
     .filter(f => f !== feature)
     .reduce<boolean>((prev, next) => prev && isFeatureEnabled(next), true);
 
